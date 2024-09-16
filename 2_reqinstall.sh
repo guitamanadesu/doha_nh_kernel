30c5e0076f37ce0d5df05b0fa97f26d462ba44da
Debian Live user <user@localhost.localdomain>
2024-09-16 23:06:00 +0000

    added missing req

--- 2_reqinstall.sh
+++ 2_reqinstall.sh
@@ -1,1 +1,1 @@
-sudo apt install android-sdk-platform-tools git wget axel bc binutils-aarch64-linux-gnu build-essential ccache curl device-tree-compiler pandoc libncurses5-dev lynx lz4 fakeroot xz-utils whiptail zip unzip -y
+sudo apt install libssl-dev android-sdk-platform-tools git wget axel bc binutils-aarch64-linux-gnu build-essential ccache curl device-tree-compiler pandoc libncurses5-dev lynx lz4 fakeroot xz-utils whiptail zip unzip -y
